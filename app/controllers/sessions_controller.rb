342d8ccd79849e5b31f5e0351f855b4a1b01ad6f
Alisher Sadikov <alishersadikov@gmailcom>
2016-12-07 13:49:01 -0700

    omni-auth integrated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,14 @@
 class SessionsController < ApplicationController
   def create
-     response = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{ENV["client_id"]}&client_secret=#{ENV["client_secret"]}&code=#{params["code"]}")
-     token = response.body.split(/\W+/)[1]
-     oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
-     auth = JSON.parse(oauth_response.body)
-
-     user          = User.find_or_create_by(uid: auth["id"])
-     user.username = auth["login"]
-     user.uid      = auth["id"]
-     user.token    = token
-     user.save
-
-    session[:user_id] = user.id
-
+    if user = User.from_omniauth(request.env["omniauth.auth"])
+      session[:user_id] = user.id
+    end
     redirect_to dashboard_index_path
+  end
+
+  def destroy
+    session.clear
+    flash[:notice] = 'Successfully logged out.'
+    redirect_to root_path
   end
 end
