e9687489544b11795e57be23ae6008e06ce96478
Alisher Sadikov <alishersadikov@gmailcom>
2016-12-07 09:41:05 -0700

    github service created

---
+++ app/services/github_service.rb
@@ -0,0 +1,11 @@
+class GithubService
+  attr_reader :token
+
+  def initialize(token)
+    @token = token
+  end
+
+  def repos
+
+  end
+end
--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 describe GithubService do
   context "#repositories" do
     it "returns all repositories for a user" do
-      repos = GithubService.new(token).repos
+      repos = GithubService.new(ENV["new_token"]).repos
+      repo = repos.first
+
+      expect(repos).to be_an(Array)
+      expect(repo).to have_key(:name)
+      expect(repo).to have_key(:full_name)
+      expect(repo).to have_key(:description)
     end
   end
 end
