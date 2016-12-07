52c8f1f27684d882e9e311f1749b98e26bceaaad
alishersadikov <alishersadikov@gmail.com>
2016-12-07 11:29:38 -0700

    add vcr

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,15 +1,17 @@
 class GithubService
-  attr_reader :token
-
   def initialize(token)
     @token = token
+    @conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.adapter Faraday.default_adapter
+      faraday.params[:access_token] = token
+    end
   end
 
   def repos
-    response = Faraday.get("https://api.github.com/user/repos?access_token=#{token}")
+    response = conn.get "/user/repos"
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
-    attr_reader :token
+    attr_reader :token, :conn
 end
