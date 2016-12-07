1764e555784010b1ff83aa2d5882bd287f7ae1f7
Alisher Sadikov <alishersadikov@gmailcom>
2016-12-07 13:18:59 -0700

    tests passing with vcr

--- spec/models/github_repo_spec.rb
+++ spec/models/github_repo_spec.rb
@@ -4,7 +4,7 @@ describe GithubRepo do
   context ".for_user(token)" do
     it "returns repos for a user" do
       VCR.use_cassette(".for_user") do
-        repos = GithubRepo.for_user(ENV["new_token"])
+        repos = GithubRepo.for_user(ENV["token"])
         repo = repos.first
 
         expect(repos).to be_an(Array)
