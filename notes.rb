e8133a6bc7985a079eadbcc4953e292ebaa61ab9
Alisher Sadikov <alishersadikov@gmailcom>
2016-12-06 22:11:41 -0700

    add rspec, capybara, figaro

--- notes.rb
+++ notes.rb
@@ -1,2 +1,24 @@
 To setup the project:
   rails new my_app -d postgresql --skip-test-unit --skip-spring -T
+
+
+RSpec setup:
+
+  - in Gemfile:
+    group :development, :test do
+      gem 'rspec-rails'
+    end
+  - from terminal:
+      rails generate rspec:install
+
+Capybara setup:
+  - in Gemfile:
+    gem 'capybara'
+  - in rails_helper.rb:
+    require 'capybara/rails'
+
+Figaro setup:
+  - in Gemfile:
+    gem 'figaro'
+  - from terminal:
+    bundle exec figaro install
