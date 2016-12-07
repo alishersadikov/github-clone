04691ce517cd74cf8c999309b05198097941fec3
Alisher Sadikov <alishersadikov@gmailcom>
2016-12-07 09:53:29 -0700

    github service complete

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -6,6 +6,10 @@ class GithubService
   end
 
   def repos
-
+    response = Faraday.get("https://api.github.com/user/repos?access_token=#{token}")
+    JSON.parse(response.body, symbolize_names: true)
   end
+
+  private
+    attr_reader :token
 end
